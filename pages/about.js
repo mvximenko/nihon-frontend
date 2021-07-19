1ae4047f0609d06c7cac5c2d7e92dbf640df7a02
mvximenko <mr.mxmnk@gmail.com>
2021-07-19 22:27:40 +0900

    Add pages and routing

---
+++ pages/about.js
@@ -0,0 +1,9 @@
+export default function AboutPage() {
+  return (
+    <div>
+      <h1>About</h1>
+      <p>This is an app to find the latest events</p>
+      <p>Version: 0.1.0</p>
+    </div>
+  );
+}
