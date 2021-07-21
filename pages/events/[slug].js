6d347f2ceb5ab3dc583ae4cfccf9ec6ff9dc18e8
mvximenko <mr.mxmnk@gmail.com>
2021-07-21 21:34:00 +0900

    Update single event page

--- pages/events/[slug].js
+++ pages/events/[slug].js
@@ -1,9 +1,61 @@
+import { FaPencilAlt, FaTimes } from 'react-icons/fa';
+import Link from 'next/link';
+import Image from 'next/image';
 import Layout from '@/components/Layout';
+import { API_URL } from '@/config/index';
+import styles from '@/styles/Event.module.css';
 
-export default function EventPage() {
+export default function EventPage({ e }) {
+  const deleteEvent = (e) => {
+    console.log('delete');
+  };
+
   return (
     <Layout>
-      <h1>My Event</h1>
+      <div className={styles.event}>
+        <div className={styles.controls}>
+          <Link href={`/events/edit/${e.id}`}>
+            <a>
+              <FaPencilAlt /> Edit Event
+            </a>
+          </Link>
+          <a href='#' className={styles.delete} onClick={deleteEvent}>
+            <FaTimes /> Delete Event
+          </a>
+        </div>
+
+        <span>{e.date}</span>
+        <h1>{e.name}</h1>
+        {e.image && (
+          <div className={styles.image}>
+            <Image src={e.image} width={960} height={600} />
+          </div>
+        )}
+
+        <h3>Description:</h3>
+        <p>{e.description}</p>
+        <h3>Venue: {e.venue}</h3>
+        <p>{e.address}</p>
+        <Link href='/events'>
+          <a className={styles.back}>{'<'} Go Back</a>
+        </Link>
+      </div>
     </Layout>
   );
+}
+
+export async function getStaticPaths() {
+  const res = await fetch(`${API_URL}/api/events`);
+  const events = await res.json();
+
+  const paths = events.map((e) => ({ params: { slug: e.slug } }));
+
+  return { paths, fallback: true };
+}
+
+export async function getStaticProps({ params: { slug } }) {
+  const res = await fetch(`${API_URL}/api/events/${slug}`);
+  const events = await res.json();
+
+  return { props: { e: events[0] }, revalidate: 1 };
 }
