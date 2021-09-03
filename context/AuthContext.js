aa5de280fb9669f551890d9be5cd5904783b6d66
mvximenko <mr.mxmnk@gmail.com>
2021-09-03 20:42:42 +0900

    Login and get JWT

--- context/AuthContext.js
+++ context/AuthContext.js
@@ -1,5 +1,6 @@
 import { createContext, useState, useEffect } from 'react';
 import { useRouter } from 'next/router';
+import { NEXT_URL } from '@/config/index';
 import { API_URL } from '@/config/index';
 
 const AuthContext = createContext();
@@ -13,7 +14,27 @@ export const AuthProvider = ({ children }) => {
   };
 
   const login = async ({ email: identifier, password }) => {
-    console.log({ identifier, password });
+    const res = await fetch(`${NEXT_URL}/api/login`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        identifier,
+        password,
+      }),
+    });
+
+    const data = await res.json();
+
+    console.log(data);
+
+    if (res.ok) {
+      setUser(data.user);
+    } else {
+      setError(data.message);
+      setError(null);
+    }
   };
 
   const logout = async () => {
